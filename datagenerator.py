f8eefb3bbc62b4dc327c6955c6bbd43a17fce02d
ChristianIngwersen <keilstrup@gmail.com>
2019-03-14 14:31:20 +0100

    Added class to generate simple synthetic data

---
+++ datagenerator.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import make_circles, make_moons, make_blobs
+
+
+class DataGen:
+
+    def circle(self, n_samples, noise):
+        x, y = make_circles(n_samples, noise)
+        return x, y
+
+    def moons(self, n_samples, noise):
+        x, y = make_moons(n_samples, noise)
+        return x, y
+
+    def gauss(self, n_samples, n_classes, dim=2):
+        x, y = make_blobs(n_samples, n_classes, dim)
+        return x, y
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    dg = DataGen()
+    n = 100
+    X, y = dg.circle(n, 0)
+
+    plt.plot(X[:int(n/2), 0], X[:int(n/2), 1], '.b')
+    plt.plot(X[int(n/2):, 0], X[int(n/2):, 1], '.r')
+
+    plt.show()
